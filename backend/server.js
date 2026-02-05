45a03c03f791499a6f56a0e7ac5c142d451c314a
Mano Adithiya V <manoadithiya2002e@gmail.com>
2026-02-05 22:37:27 +0530

    Verify local PostgreSQL connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const pool=require("./src/config/db")
 
 const authRoutes = require("./src/routes/auth");
 const pricelistRoutes = require("./src/routes/pricelist");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*", // later restrict to your frontend domain
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
