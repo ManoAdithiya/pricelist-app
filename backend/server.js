f6e6864e8b1d37d9533ff05b0f5bf30444e85745
Mano Adithiya V <manoadithiya2002e@gmail.com>
2026-02-08 20:31:27 +0530

    Database Update

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,16 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/pricelist", pricelistRoutes);
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pricelist-app-mu.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
 });
